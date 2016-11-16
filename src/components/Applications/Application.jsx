02eaad18c69101cf602ba4307e4f947c7103a01b
tixtixti <tktuononen@hotmail.com>
2016-11-16 15:09:49 +0200

    some view logic to application and modal rework

--- src/components/Applications/Application.jsx
+++ src/components/Applications/Application.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fromJS } from 'immutable';
-
-import { getApplicationData } from '../../actions/action_creators';
+import { Router } from 'react-router'
+import { getApplicationData, deleteApplication } from '../../actions/action_creators';
 import { Header } from "../generals/Generals.jsx";
-
+import { ModalClass, openModal } from "../generals/Modals.jsx";
 export const Experiments = ({ name, id, starts, ends}) => {
 
   return (
@@ -20,41 +20,85 @@ export const Experiments = ({ name, id, starts, ends}) => {
 
 
 export class ApplicationBase extends Component {
+
   componentWillMount() {
     let id  = this.props.location.pathname.replace( /^\D+/g, ''); //strip anything but numbers
     this.props.onLoad(id);
   }
+  notYetReadyRender(){
+    return ( <div className="ApplicationHeader">
+        <h3>Loading ... </h3>
+      </div> )
+  }
 
   render() {
-    var testDate = Date.now();
+    const id  = this.props.location.pathname.replace( /^\D+/g, '');
+    const index = this.props.apps.findIndex(map=>{ return map.get('id') === parseInt(id,10)  });
+
+    if ( index === -1 ) return this.notYetReadyRender() //indicates we have not yet loaded app
+
+    const thisApp = this.props.apps.get(index).toJS(); //plainJS or immutableJS? If we only access it ?
+
+    if( !(thisApp.configurationkeys) ) return this.notYetReadyRender() //indicates we have not yet loaded app details
+
     return(
       <div>
         <div className="ApplicationHeader">
-          <h3>Applikaation otsikkos</h3>
+          <h3>{thisApp.name}</h3>
         </div>
 
         <div className="SubHeader">
-          <h3>Experiments</h3>
+          <h3>Overview</h3>
         </div>
 
         <div className="Applications">
-          <Experiments
-            name={"Kokeen nimi"}
-            id={1}
-            starts={testDate}
-            ends={testDate}
-            clientCount={42} />
+          <div>
+            <h4>Configuration Keys</h4>
+            {thisApp.configurationkeys.map(key =>{
+              return(<div key={key.id}>
+                {key.name} : {key.type}
+              </div>)
+            })}
+          </div>
         </div>
+
+          <h3>Configuration Keys</h3>
+        <div className="Applications">
+          <div>
+            <h4>Configuration Keys</h4>
+            {thisApp.configurationkeys.map(key =>{
+              return(<div key={key.id}>
+                {key.name} : {key.type} <button>delete</button>
+              </div>)
+            })}
+            <button>add new one </button>
+          </div>
+        </div>
+        <h3>Danger Zone</h3>
+      <div className="Applications">
+        <div>
+          <h4>Delete this Application</h4>
+            <ModalClass
+              modalId="deleteApplication"
+              textContent={"Are you sure you want to delete " + thisApp.name }
+              buttons={ [ { text: "deleteFromUpper", action: ()=> this.props.onDeleteClick(thisApp.id) }]  }  />
+            <button onClick={() => openModal("deleteApplication")}>Delete this app </button>
+        </div>
+      </div>
 
       </div>
     )
   }
 }
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = (dispatch, ownProps) => {
   return {
     onLoad: (id) => {
       dispatch(getApplicationData(id))
+    },
+    onDeleteClick: (id) => {
+      ownProps.router.push("/applications")
+      dispatch(deleteApplication(id));
     }
   }
 }
@@ -63,9 +107,9 @@ const mapDispatchToProps = (dispatch) => {
  * CONSIDER to refactor apps state to ordeder set and invent better algorithm.
  */
 function mapStateToProps(state) {
-  if(!(state.get('applications').isEmpty())){
-    let apps = state.get('applications').get('apps');
-    return { application: (state.get('applications').get('apps')) }
+
+  if((state.get('applications').get('apps'))){
+    return { apps: (state.get('applications').get('apps')) }
   } else {
     return {apps: fromJS([{id:100, name:"Loading"}])}
   }
