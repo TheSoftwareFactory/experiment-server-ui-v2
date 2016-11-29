3d3a317b6269cad4e76676806b64ceef73ab27f1
tixtixti <tktuononen@hotmail.com>
2016-11-29 12:35:28 +0200

    better rangeConstraintpost

--- src/components/Applications/applicationComponents/RangeConstraints.jsx
+++ src/components/Applications/applicationComponents/RangeConstraints.jsx
@@ -5,44 +5,111 @@ import { postRangeKey, deleteRangeKey }  from '../../../actions/applicationAsycA
 import { DataBox } from '../../generals/Generals.jsx'
 import { Modal, openModal, closeModal } from "../../generals/Modal.jsx";
 
+
+
 export class RangeConstraints extends Component{
 
-  findRightConfigurationKey(app, configKeyId){
-    return app.configurationkeys.filter(key=>{
+  findRightConfigurationKey(configKeyId){
+    return this.props.app.configurationkeys.filter(key=>{
         if(key.id === configKeyId){
           return key;
         }
     })[0]
   }
+
+  chooseInputType(keyId){
+    const key = this.findRightConfigurationKey(parseInt(keyId,10));
+
+    if(key.type === "Integer"){
+      return   (<div><input ref="value" type={"number"} /> Integer</div>)
+    }
+    if(key.type=== "boolean"){
+      return   (<form>
+          <input ref="value" type="radio" name="true" value="true" />True
+          <input ref="value" type="radio" name="false" value="false" />False
+         </form>)
+    }
+    if(key.type=== "String"){
+      return   (<div><input ref="value" type="text" /> Text</div>)
+    }
+    if(key.type=== "Float"){
+      return   (<div><input ref="value" type="number" step="0.01" /> Float (0.01)</div>)
+    }
+
+  }
+  listRanges(rconst){
+    return(<div key={rconst.id}>
+      {this.findRightConfigurationKey(rconst.configurationkey_id).name}:
+    {this.props.operations[rconst.operator_id -1].human_value} :
+  {rconst.value}
+  <button onClick={() => this.props.onRangeDeleteClick(this.props.app.id,
+      rconst.configurationkey_id,
+       rconst.id) }>Delete heres</button>
+ </div>)
+  }
+  postButton(){
+    return(<p>
+      <button onClick={() => this.props.onRangeClick(this.refs.constkey.value, this.refs.operator.value, this.refs.value.value, this.props.app.id )}>Post Range</button>
+    </p>)
+  }
+
+  chooseOperation(){
+    return(<select ref="operator"> {this.props.operations.map(op=>{
+      if(op.id > 1 && op.id < 6 ){
+          return (<option key={"opRange" + op.id} value={op.id} >{op.human_value}</option>)
+      }
+    })}
+  </select>)
+  }
+  /**
+   * To create ui for adding new range key.
+   * uses React forceUpdate to update component view.
+   * TODO find more reactive way to handle this.
+   */
+  addNewRange(){
+    let constkey;
+    try {
+      constkey = this.refs.constkey.value;
+    } catch (e) {
+      constkey = this.props.app.configurationkeys[0].id
+    } finally {
+      return (<div> <select onChange={()=>this.forceUpdate()} ref="constkey">
+        {this.props.app.configurationkeys.map(key=>{
+          if(key.type !== "boolean"){
+            return (<option value={key.id} key={key.id + "configKey"}>
+                {key.name}
+              </option> )}})}
+
+
+       </select>
+       {this.chooseOperation()}
+       {this.chooseInputType(constkey)}
+       </div>)
+    }
+  }
+/*
+
+ <input type={this.chooseInputType()} />
+*/
+
+  /**
+   * so here we need to make <select> tag for each kind of key name
+   * and then based on selected name we need to select input type.
+   * and then maybe make this a function.
+  * FILTER something somethig something.
+   */
   render(){
     return(
       <DataBox heading="Range Constrains"
         content={
           <div>
-              {this.props.app.rangeconstraints.map(rconst=>{
-                return (<div key={rconst.id}>
-                  {this.findRightConfigurationKey(this.props.app,rconst.configurationkey_id).name}:
-                {this.props.operations[rconst.operator_id -1].human_value} :
-              {rconst.value}
-              <button onClick={() => this.props.onRangeDeleteClick(this.props.app.id,
-                  rconst.configurationkey_id,
-                   rconst.id) }>Delete heres</button>
-             </div>)
-              })}
-              <select ref="constkey">
-                {this.props.app.configurationkeys.map(key=>{
-                  return <option key={"key" + key.id} value={key.id}>{key.name}</option>
-                })}
-              </select>
-              <select ref="operator">
-                {this.props.operations.map(op=>{
-                  return <option key={"op"+op.id} value={op.id}>{op.human_value}</option>
-                })}
-              </select>
-              <input type="text" ref="value" placeholder="value"></input>
-              <p>
-              <button onClick={() => this.props.onRangeClick(this.refs.constkey.value, this.refs.operator.value, this.refs.value.value, this.props.app.id )}>Post Range</button>
-            </p></div>
+            {this.props.app.rangeconstraints.map(rconst=>{
+              return (this.listRanges(rconst))
+            })}
+             {this.addNewRange()}
+            {this.postButton()}
+
+          </div>
         } />
     )
   }
